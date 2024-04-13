fb12e163e72bf2a2fb66444c1f75b94a9fc79309
Pradeep Roark <pradeep.roark@gmail.com>
2024-04-13 15:41:53 +0100

    Tags with capitalcase are rendered as components

--- src/pixieverse/render_html/render.py
+++ src/pixieverse/render_html/render.py
@@ -2,8 +2,5 @@ from typing import List
 
 
 def createElement(elem: str, props: dict[str, str] = {}, children: List[str] = []):
-    if elem[0:1].islower():
-        render_children = "".join(children)
-        return f"<{elem}>{render_children}</{elem}>"
-    else:
-        raise NotImplementedError
+    render_children = "".join(children)
+    return f"<{elem}>{render_children}</{elem}>"
--- src/tests/test_render.py
+++ src/tests/test_render.py
@@ -63,3 +63,62 @@ print(comp)
             except Exception as ex:
                 self.fail(f"Unable to execute source {ex}")
         self.assertEqual(expected, output.getvalue())
+
+    def test_RenderMultipleNested(self):
+        input = """
+from src.pixieverse.render_html.render import createElement
+comp=<p><h1></h1><h2></h2></p>
+print(comp)
+"""
+        expected = """<p><h1></h1><h2></h2></p>
+"""
+        source = transpile_source(input)
+        with stdoutIO() as output:
+            try:
+                exec(source, globals())
+            except Exception as ex:
+                self.fail(f"Unable to execute source {ex}")
+        self.assertEqual(expected, output.getvalue())
+
+    def test_RenderComponent(self):
+        input = """
+from src.pixieverse.render_html.render import createElement
+
+def header_comp(tagName, props, children):
+    return (<p><h1></h1><h2></h2></p>)
+
+comp=<header_comp></header_comp>
+print(comp)
+"""
+        expected = """<p><h1></h1><h2></h2></p>
+"""
+        source = transpile_source(input)
+        with stdoutIO() as output:
+            try:
+                exec(source, globals())
+            except Exception as ex:
+                self.fail(f"Unable to execute source: {ex}")
+        self.assertEqual(expected, output.getvalue())
+
+    def test_RenderComponentChildren(self):
+        input = """
+from src.pixieverse.render_html.render import createElement
+
+def menu_comp(tagName, props, children):
+    return <ul><li></li></ul>
+
+def header_comp(tagName, props, children):
+    return (<h1><menu_comp></menu_comp></h1>)
+
+comp=<header_comp></header_comp>
+print(comp)
+"""
+        expected = """<h1><ul><li></li></ul></h1>
+"""
+        source = transpile_source(input)
+        with stdoutIO() as output:
+            try:
+                exec(source, globals())
+            except Exception as ex:
+                self.fail(f"Unable to execute source: {ex}")
+        self.assertEqual(expected, output.getvalue())
