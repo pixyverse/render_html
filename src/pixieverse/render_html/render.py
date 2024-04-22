09b1429f321e95cec7f0f5120c84cd85768e0d14
Pradeep Roark <pradeep.roark@gmail.com>
2024-04-22 15:24:53 +0100

    Test if can render simple elements
    Render nested components

--- src/pixieverse/render_html/render.py
+++ src/pixieverse/render_html/render.py
@@ -2,4 +2,8 @@ from typing import List
 
 
 def createElement(elem: str, props: dict[str, str] = {}, children: List[str] = []):
-    pass
+    if elem[0:1].islower():
+        render_children = "".join(children)
+        return f"<{elem}>{render_children}</{elem}>"
+    else:
+        raise NotImplementedError
--- src/tests/test_render.py
+++ src/tests/test_render.py
@@ -1,6 +1,65 @@
+import contextlib
+from io import StringIO
+import sys
 import unittest
+from pixieverse.pixie.transpile import transpile_source
+
+
+@contextlib.contextmanager
+def stdoutIO(stdout=None):
+    old = sys.stdout
+    if stdout is None:
+        stdout = StringIO()
+    sys.stdout = stdout
+    yield stdout
+    sys.stdout = old
 
 
 class TestRender(unittest.TestCase):
     def test_RenderMinimal(self):
-        pass
+        input = """
+from src.pixieverse.render_html.render import createElement
+comp=<p></p>
+print(comp)
+"""
+        expected = """<p></p>
+"""
+        source = transpile_source(input)
+        with stdoutIO() as output:
+            try:
+                exec(source, globals())
+            except Exception as ex:
+                self.fail(f"Unable to execute source {ex}")
+        self.assertEqual(expected, output.getvalue())
+
+    def test_RenderNested(self):
+        input = """
+from src.pixieverse.render_html.render import createElement
+comp=<p><h1></h1></p>
+print(comp)
+"""
+        expected = """<p><h1></h1></p>
+"""
+        source = transpile_source(input)
+        with stdoutIO() as output:
+            try:
+                exec(source, globals())
+            except Exception as ex:
+                self.fail(f"Unable to execute source {ex}")
+        self.assertEqual(expected, output.getvalue())
+
+    def test_RenderMultipleNested(self):
+        input = """
+from src.pixieverse.render_html.render import createElement
+comp=<p><h1></h1><h2></h2></p>
+print(comp)
+"""
+        expected = """<p><h1></h1><h2></h2></p>
+"""
+        source = transpile_source(input)
+        with stdoutIO() as output:
+            try:
+                exec(source, globals())
+            except Exception as ex:
+                self.fail(f"Unable to execute source {ex}")
+        self.assertEqual(expected, output.getvalue())
