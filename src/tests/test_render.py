2e061516c4fcf71726ac6e9420eef8b03dfb1232
Pradeep Roark <pradeep.roark@gmail.com>
2024-05-20 18:59:59 +0100

    Match call to create_element snakecase change in pixy dependency

--- src/tests/test_render.py
+++ src/tests/test_render.py
@@ -19,7 +19,7 @@ def stdoutIO(stdout=None):
 class TestRender(unittest.TestCase):
     def test_render_minimal(self):
         input = """
-from src.pixyverse.render_html.render import createElement
+from src.pixyverse.render_html.render import create_element
 comp=<p></p>
 print(comp)
 """
@@ -35,7 +35,7 @@ print(comp)
 
     def test_render_nested(self):
         input = """
-from src.pixyverse.render_html.render import createElement
+from src.pixyverse.render_html.render import create_element
 comp=<p><h1></h1></p>
 print(comp)
 """
@@ -51,7 +51,7 @@ print(comp)
 
     def test_render_multiple_nested(self):
         input = """
-from src.pixyverse.render_html.render import createElement
+from src.pixyverse.render_html.render import create_element
 comp=<p><h1></h1><h2></h2></p>
 print(comp)
 """
@@ -67,7 +67,7 @@ print(comp)
 
     def test_render_literal_strings(self):
         input = """
-from src.pixyverse.render_html.render import createElement
+from src.pixyverse.render_html.render import create_element
 comp=<p>"Hello World"</p>
 print(comp)
 """
@@ -83,7 +83,7 @@ print(comp)
 
     def test_render_component(self):
         input = """
-from src.pixyverse.render_html.render import createElement
+from src.pixyverse.render_html.render import create_element
 
 def header_comp(tagName, props, children):
     return (<p><h1></h1><h2></h2></p>)
@@ -103,7 +103,7 @@ print(comp)
 
     def test_render_component_children(self):
         input = """
-from src.pixyverse.render_html.render import createElement
+from src.pixyverse.render_html.render import create_element
 
 def menu_comp(tagName, props, children):
     return <ul><li></li></ul>
@@ -126,7 +126,7 @@ print(comp)
 
     def test_render_mixed_literal_component_children(self):
         input = """
-from src.pixyverse.render_html.render import createElement
+from src.pixyverse.render_html.render import create_element
 
 def menu_comp(tagName, props, children):
     return <ul><li></li></ul>
@@ -154,7 +154,7 @@ print(comp)
 
     def test_render_props(self):
         input = """
-from src.pixyverse.render_html.render import createElement
+from src.pixyverse.render_html.render import create_element
 comp=<p title="important info">"Buy 2 for 1"</p>
 print(comp)
 """
@@ -174,7 +174,7 @@ print(comp)
 
     def test_render_unsafe_props(self):
         input = """
-from src.pixyverse.render_html.render import createElement
+from src.pixyverse.render_html.render import create_element
 comp=<p class_name="highlight">"Buy 2 for 1"</p>
 print(comp)
 """
@@ -190,7 +190,7 @@ print(comp)
 
     def test_render_list_expressions(self):
         input = """
-from src.pixyverse.render_html.render import createElement
+from src.pixyverse.render_html.render import create_element
 comp=<p>{[<li>{fruit}</li> for fruit in ['apple','mango','pear']]}</p>
 print(comp)
 """
@@ -206,7 +206,7 @@ print(comp)
 
     def test_render_unsafe_props(self):
         input = """
-from src.pixyverse.render_html.render import createElement
+from src.pixyverse.render_html.render import create_element
 comp=<p class_name="highlight">"Buy 2 for 1"</p>
 print(comp)
 """
@@ -222,7 +222,7 @@ print(comp)
 
     def test_render_props_underscores(self):
         input = """
-from src.pixyverse.render_html.render import createElement
+from src.pixyverse.render_html.render import create_element
 comp=<p class_name="highlight" data_touch="multi">"Buy 2 for 1"</p>
 print(comp)
 """
