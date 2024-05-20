d810665e44b0c83aa594e50769e1983e12d0dfd5
Pradeep Roark <pradeep.roark@gmail.com>
2024-05-20 19:27:36 +0100

    convert underscores in attribute names hyphens as underlying grammar does not support hyphens in names. TODO: what if we want the underscores to remain?

--- src/pixyverse/render_html/render.py
+++ src/pixyverse/render_html/render.py
@@ -19,10 +19,14 @@ def flat_map(f, xs: Iterable):
     return ys
 
 
-def createElement(
+def underscoreToHyphen(input: str) -> str:
+    return input.replace("_", "-")
+
+
+def create_element(
     elem: str,
     props: dict[str, str] = {},
     children: Iterable[str] | Iterable[Iterable[str]] = [],
 ):
     render_children = "".join(flat_map(id, children))
-    return f"""<{elem}{''.join(f' {map_prop_name(key)}={value}' for key, value in props.items())}>{render_children}</{elem}>"""
+    return f"""<{elem}{''.join(f' {underscoreToHyphen(map_prop_name(key))}={value}' for key, value in props.items())}>{render_children}</{elem}>"""
